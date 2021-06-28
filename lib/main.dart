81bda4a98a0596180b41966a54bc6c6c3fe8af78
준성 <madox9999@gmail.com>
2021-06-28 14:24:19 +0900

    HomePage_구성 및 폰트변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_it_traplan/repository/repository.dart';
 import 'package:flutter_it_traplan/ui/home_page/home.dart';
+import 'package:provider/provider.dart';
 
 
 void main() {
@@ -9,22 +11,18 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    Widget build(BuildContext context) {
+      return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<TraplanProvider>(
+              create: (_) => TraplanProvider()),
+        ],child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Home(),
       ),
-      home: Home(),
-    );
+      );
+    }
   }
-}
